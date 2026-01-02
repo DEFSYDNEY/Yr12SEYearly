7cf6843da01708bdd4a719c1320a1dfdcb94742a
DefsydneyNunno <bobrosstress@gmail.com>
2026-01-02 17:44:06 +1100

    Reverted back to most of the same
    Fixed to have the same game as before I tried changes

--- Testing/Resources/code/weapon_stats.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name WeaponStats
-
-@export var name: String
-@export var damage: int = 10
-@export var posture_damage: int = 15
-@export var attack_speed: float = 1.0
-@export var sprite: Texture2D
